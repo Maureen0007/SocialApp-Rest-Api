c2a622d7e885cd329d4d4101534a58e8bef2b3a5
Maureen <maureenwanjiku85@outlook.com>
2025-02-06 19:01:48 +0300

    add CRUD operations

--- routes/auth.js
+++ routes/auth.js
@@ -20,7 +20,30 @@ router.post("/register", async (req,res)=>{
         const user = await newUser.save();
         res.status(200).json(user);
     } catch(err) {
-        console.log(err);
+        res.status(500).json("Internal Server Error");
     }
 });
+
+
+// login
+router.post("/login", async(req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            return res.status(404).json("User not found");
+        }
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        if (!validPassword) {
+            return res.status(400).json("Incorrect password");
+        }
+
+        // Ensure that the response is only sent once
+        return res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json("Internal Server Error");
+    }
+});
+
+
 module.exports = router;
