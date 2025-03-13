f526d272bff91ec48579c5efd6d4d1ae5abe54fd
Maureen <maureenwanjiku85@outlook.com>
2025-03-13 16:13:55 +0300

    final commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const postRoute = require("./routes/posts");
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URI)
     console.log("Connected to MongoDB");
 
 
