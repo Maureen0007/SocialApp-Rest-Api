35907e33a2404597a63bf68c7435a047598957f2
Maureen <maureenwanjiku85@outlook.com>
2025-03-13 17:18:51 +0300

    final commit

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ mongoose.connect(process.env.MONGO_URI)
       
     //   default
     app.get("/", (req, res) => {
-        res.send("Welcome to the SocialApp API! Enter your preferred endpoint!");
+        res.send("Welcome to the SocialApp API! Enter your preferred endpoint Up on the search bar ☝️!");
     });
     
 
