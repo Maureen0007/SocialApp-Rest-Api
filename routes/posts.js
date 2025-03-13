27ffbd72a8f795b2799a5a0493a5af3a78eaac1d
Maureen <maureenwanjiku85@outlook.com>
2025-03-13 14:16:59 +0300

    final commit

--- routes/posts.js
+++ routes/posts.js
@@ -63,31 +63,40 @@ router.put("/:id/like", async (req,res)=>{
 })
 // get a post
 
-router.get("/:id", async(req, res)=>{
-    try{
-        const post = await Post.findById(req.params.id);
-        res.status(200).json(post)
-    }catch(err){
-        res.status(500).json(err)
+router.get("/:id", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      res.status(200).json(post);
+    } catch (err) {
+      res.status(500).json(err);
     }
-})
+  });
 // get timeline posts
 
-router.get("/timeline/all", async (req, res) => {
+router.get("/timeline/:userId", async (req, res) => {
     try {
-      const currentUser = await User.findById(req.body.userId);
+      const currentUser = await User.findById(req.params.userId);
       const userPosts = await Post.find({ userId: currentUser._id });
       const friendPosts = await Promise.all(
         currentUser.followins.map((friendId) => {
           return Post.find({ userId: friendId });
         })
       );
-      res.json(userPosts.concat(...friendPosts))
+      res.status(200).json(userPosts.concat(...friendPosts));
     } catch (err) {
       res.status(500).json(err);
     }
   });
 
-
+//   get user's all posts
+router.get("/profile/:username", async (req, res) => {
+    try {
+      const user = await User.findOne({ username: req.params.username });
+      const posts = await Post.find({ userId: user._id });
+      res.status(200).json(posts);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;   
